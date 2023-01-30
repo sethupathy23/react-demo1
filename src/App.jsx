aaa3436a3868c1e714e36f96ee94de8b276786a9
sethupathy23 <sethupathy06@gmail.com>
2023-01-30 11:38:21 +0530

    toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { AddColor } from "./AddColor";
 import { MovieList } from "./MovieList";
 import { Counter } from "./Counter";
 import { TicTacToe } from "./TicTacToe";
+import { Routes, Route, Link } from "react-router-dom";
+
  export default function App() {
  return (
     <div className="App">
@@ -10,8 +12,22 @@ import { TicTacToe } from "./TicTacToe";
       {/* <AddColor /> */}
       {/* <Counter /> */}
       <TicTacToe />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/TicTacToe" element={<TicTacToe />} />
+      </Routes>
     </div>
       );
+   }
+
+   function Home() {
+    return (
+      <div>
+        <h1>
+          Welcome to the movie app 😍
+        </h1>
+      </div>
+    );
    }
 
 
